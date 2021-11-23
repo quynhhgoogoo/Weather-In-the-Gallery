a2e13ec2d2f7f36a1df533fe9922420cc3af6886
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-11-23 22:38:11 +0200

    send email based on condition

--- send_email.py
+++ send_email.py
@@ -2,21 +2,19 @@
 
 import smtplib
 
-sender = 'weather.in.gallery@gmail.com'
-receiver = 'luongdiemquynh1998@gmail.com'
-password="weatherinthegallery"
 
-message = """From: Weather in the Gallery team <weather.in.gallery@gmail.com>
-To: To Person <luongdiemquynh1998@gmail.com>
-Subject: Warning: Abnormal values on Sensor
+def send_email(message):
+    ''' Send an email to user when abnormal activities is detected '''
+    
+    sender = 'weather.in.gallery@gmail.com'
+    receiver = 'luongdiemquynh1998@gmail.com'
+    password= "weatherinthegallery"
 
-There are some abnormal values observed from sensor. Please check it out carefully!
-"""
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender, password)
+    print("Login suceed!")
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender, password)
-print("Login suceed!")
-
-server.sendmail(sender, receiver, message)
-print("Email has sent to", receiver)
+    server.sendmail(sender, receiver, message)
+    print("Email has sent to", receiver)
+    print(message)
--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from send_email import *
+
+
+# Define range of sensor's value in normal cases
+TEMPERATURE_MIN, TEMPERATURE_MAX = 15, 26
+HUDMIDITY_MIN, HUDMIDITY_MAX = 40, 50
+ACCELERATOR_X, ACCELERATOR_Y, ACCELERATOR_Z = 1, 1, 1
+
 
 '''
 # Download from URL and decode as UTF-8 text.
@@ -22,3 +30,20 @@ f.close()
 sensor_values = content_list[0][:-1].split(",")
 sensor_values = [float(i) for i in sensor_values]
 print(sensor_values)
+
+# Check if sensor values is abnormal and send a notify email to user
+message = """From: Weather in the Gallery team <weather.in.gallery@gmail.com>
+To: To Person <luongdiemquynh1998@gmail.com>
+Subject: Warning: Abnormal values on Sensor
+There are some abnormal values observed from sensor. Please check it out carefully!
+"""
+
+if (sensor_values[0] < TEMPERATURE_MIN) or (sensor_values[0] > TEMPERATURE_MAX):
+    message += """Current temperature is out of range """ + str(sensor_values[0]) + "\n"
+if (sensor_values[1] < HUDMIDITY_MIN) or (sensor_values[1] > HUDMIDITY_MAX):
+    message += """Current hudmidy is out of range """ + str(sensor_values[1]) + "\n"
+if (sensor_values[2] < ACCELERATOR_X) or (sensor_values[3] > ACCELERATOR_Y) or (sensor_values[4] > ACCELERATOR_Z):
+    message += """Current accelerator is too low """ + str(sensor_values[2]) + "," + str(sensor_values[3]) + ","  + str(sensor_values[4]) + "\n"
+
+if len(message) != 0:
+    send_email(message)
